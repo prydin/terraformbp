b2f448e42ac3eb4fb7a9112e39ce8683b1c57c80
prydin <prydin@vmware.com>
2021-05-26 12:08:16 -0400

    Finalized Python example

--- aws-with-python/main.tf
+++ aws-with-python/main.tf
@@ -13,10 +13,15 @@ variable "myTag" {
   default = "terraform-test"
 }
 
+# Dummy resource that runs a provisioner to install Python. Make sure
+# resources needing Python declare this as a "depends_on".
 resource "null_resource" "install_python" {
   provisioner "local-exec" {
     command = "apk add --no-cache python3 py3-pip; python3 -c 'print(\"Python successfully installed!\")'"
   }
+
+  # This is a trick to make sure the installation runs regardless of the 
+  # type of operation.
   triggers = {
     always_run = timestamp()
   }
@@ -28,9 +33,13 @@ resource "aws_instance" "machine1" {
   tags = {
     "type" = var.myTag
   }
+  
+  # Test that python is indeed available. Replace this with whatever Python code you
+  # need to run.
   provisioner "local-exec" {
     command = "python3 -c 'print(\"Python is accessible from resource provisioning!\")'"
  }
+  # Makes sure Python is installed before we provision
   depends_on = [ null_resource.install_python ]
 }
 
