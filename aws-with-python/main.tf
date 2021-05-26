fa43a19f73496c9d2fc95ed3a39dd62c509de980
prydin <prydin@vmware.com>
2021-05-26 11:33:34 -0400

    WIP

--- aws-with-python/main.tf
+++ aws-with-python/main.tf
@@ -13,14 +13,24 @@ variable "myTag" {
   default = "terraform-test"
 }
 
-resource "aws_instance" "machine1" {
-    ami           = "ami-0a63cd87767e10ed4"
-    instance_type = "t2.micro"
-#    availability_zone = "us-west-1b"
-    tags = {
-      "type" = var.myTag
-    }
-    provisioner "local-exec" {
-	command = "apk add --no-cache python3 py3-pip; python3 -c 'print(\"Hello\")'"
+resource "null_resource" "install_python" {
+  provisioner "local-exec" {
+    command = "apk add --no-cache python3 py3-pip; python3 -c 'print(\"Python successfully installed!\")'"
+  }
+  triggers = {
+    always_run = timestamp()
   }
 }
+
+resource "aws_instance" "machine1" {
+  ami           = "ami-0a63cd87767e10ed4"
+  instance_type = "t2.micro"
+  tags = {
+    "type" = var.myTag
+  }
+  provisioner "local-exec" {
+    command = "python3 -c print('Python is accessible from resource provisioning!')"
+ }
+  depends_on = [ null_resource.install_python ]
+}
+
